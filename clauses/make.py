9a2511b77235c02ff681a825419ab609fcc6a190
Harry Watkinson <harry@revmo.app>
2022-10-17 20:07:50 +0100

    parsing string

--- clauses/make.py
+++ clauses/make.py
@@ -1,16 +1,19 @@
-from re import compile
-
-
-s = compile(
-    r"""(?P<clause>[MmAaKkEe]{4})\s?\(
-(?P<handle>\w+)\s?:(?P<node_label>\w+)\)"""
-)
-
-"""
-MAKE (node:NodeLable)
-
-"""
-
-
-def make():
-    pass
+# import re
+#
+# from re import search
+#
+# pattern = r"""(?P<clause>[MmAaKkEe]{4})\s?\((?P
+# <handle>\w+)\s?:(?P<node_label>\w+)\)"""
+#
+# string = "MAKE (node:NodeLabel)"
+#
+#
+# def make(input_string: str):
+#
+#     # if match := re.search(pattern, string):
+#     #
+#     #     clause = match.group("clause")
+#     #     handle = match.group("handle")
+#     #     node_label = match.group("node_label")
+#
+#     raise Exception
