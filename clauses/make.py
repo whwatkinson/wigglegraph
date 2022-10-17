f51ebdb3ace07d45f5d84ea110b64e07f438af1e
Harry Watkinson <harry@revmo.app>
2022-10-17 19:50:50 +0100

    moved make to right file

--- clauses/make.py
+++ clauses/make.py
@@ -1,2 +1,16 @@
-def create():
+from re import compile
+
+
+s = compile(
+    r"""(?P<clause>[MmAaKkEe]{4})\s?\(
+(?P<handle>\w+)\s?:(?P<node_label>\w+)\)"""
+)
+
+"""
+MAKE (node:NodeLable)
+
+"""
+
+
+def make():
     pass
