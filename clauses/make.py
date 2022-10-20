c09724fcca9e3d8aef63d165a0784484cc97b9be
harrywatkinson <harry@revmo.app>
2022-10-20 14:47:46 +0100

    wip

--- clauses/make.py
+++ clauses/make.py
@@ -2,18 +2,26 @@
 #
 # from re import search
 #
-# pattern = r"""(?P<clause>[MmAaKkEe]{4})\s?\((?P
-# <handle>\w+)\s?:(?P<node_label>\w+)\)"""
+# pattern = r"""(?P<clause>[MmAa
+# KkEe]{4})\s?\((?P<handle>\w+)\s?:(?P<node_label>\w+)\)"""
 #
 # string = "MAKE (node:NodeLabel)"
 #
 #
 # def make(input_string: str):
+#     pass
 #
-#     # if match := re.search(pattern, string):
-#     #
-#     #     clause = match.group("clause")
-#     #     handle = match.group("handle")
-#     #     node_label = match.group("node_label")
+#
+# def parse_make_statement(input_string: str):
+#
+#     if match := re.search(pattern, input_string):
+#         clause = match.group("clause")
+#         if clause.upper() != "MAKE":
+#             raise Exception
+#
+#         handle = match.group("handle")
+#         node_label = match.group("node_label")
+#
+#         return clause, handle, node_label
 #
 #     raise Exception
--- clauses/tests/test_make.py
+++ clauses/tests/test_make.py
@@ -1,3 +1,27 @@
-class TestMake:
-    def test_make(self):
-        assert True
+# from typing import Generator
+# from contextlib import contextmanager
+#
+# import pytest
+#
+# from clauses.make import parse_make_statement
+#
+# @contextmanager
+# def does_not_raise():
+#     yield
+#
+#
+# class TestMake:
+#
+#     @pytest.mark.parametrize(
+#         "input_string, expected_result, exception",
+#         [
+#             ("MAKE (node:NodeLabel)", (1, 2, 3), does_not_raise())
+#         ]
+#     )
+#     def test_parse_make_statement(self, input_string:
+#     str, expected_result:, exception: Generator):
+#         with exception:
+#
+#             test = parse_make_statement(input_string)
+#
+#             assert t
