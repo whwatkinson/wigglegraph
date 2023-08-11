4a012f6358f02f82555088109cabd75bdf054195
William Watkinson <w.h.watkinson@gmail.com>
2023-08-11 20:04:14 +0100

    correctly emiting nodes and rels!

--- models/wql/clauses/make/make_pre.py
+++ models/wql/clauses/make/make_pre.py
@@ -25,3 +25,25 @@ class MakePre(BaseModel):
     right_node: Optional[NodePre]
     left_middle_relationship: Optional[RelationshipPre]
     middle_right_relationship: Optional[RelationshipPre]
+
+    def emit_node_rels(self) -> tuple:
+        # TODO make this a model not a tuple
+
+        lm_rel = self.left_middle_relationship
+        mr_rel = self.middle_right_relationship
+
+        return (
+            (
+                self.left_node,
+                lm_rel if lm_rel.wn_from_node == self.left_node.wn else None,
+            ),
+            (
+                self.middle_node,
+                lm_rel if lm_rel.wn_from_node == self.middle_node.wn else None,
+                mr_rel if mr_rel.wn_from_node == self.middle_node.wn else None,
+            ),
+            (
+                self.right_node,
+                mr_rel if mr_rel.wn_from_node == self.right_node.wn else None,
+            ),
+        )
