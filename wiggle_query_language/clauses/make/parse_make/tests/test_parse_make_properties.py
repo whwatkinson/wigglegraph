dd1545ee02155434abdd28dbf2103318d221490e
William Watkinson <w.h.watkinson@gmail.com>
2023-08-13 19:56:35 +0100

    failing test

--- wiggle_query_language/clauses/make/parse_make/tests/test_parse_make_properties.py
+++ wiggle_query_language/clauses/make/parse_make/tests/test_parse_make_properties.py
@@ -10,31 +10,36 @@ from wiggle_query_language.clauses.make.parse_make.parse_make_properties import 
 
 class TestParseMakeProperties:
     @pytest.mark.parametrize(
-        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+        "test_stmt, expected_value, exception",
+        [
+            pytest.param("", "", does_not_raise(), id="EXP PASS: "),
+        ],
     )
     def test_make_properties(
         self, test_stmt: str, expected_value: dict, exception
     ) -> None:
         with exception:
             test = make_properties(test_stmt)
-            assert test == dict()
+            assert test == expected_value
 
     @pytest.mark.parametrize(
-        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+        "test_stmt, expected_value, exception",
+        [pytest.param("", "", does_not_raise(), id="EXP PASS: ")],
     )
     def test_string_to_correct_data_type(
         self, test_stmt: str, expected_value: dict, exception
     ) -> None:
         with exception:
             test = string_to_correct_data_type(test_stmt)
-            assert test == dict()
+            assert test == expected_value
 
     @pytest.mark.parametrize(
-        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+        "test_stmt, expected_value, exception",
+        [pytest.param("", "", does_not_raise(), id="EXP PASS: ")],
     )
     def test_string_to_list_data_type(
         self, test_stmt: str, expected_value: dict, exception
     ) -> None:
         with exception:
             test = string_to_list_data_type(test_stmt)
-            assert test
+            assert test == expected_value
