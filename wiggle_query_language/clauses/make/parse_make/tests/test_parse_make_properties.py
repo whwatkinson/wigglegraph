d9df890b082ae260fff6feb1c22503588ff1c316
William Watkinson <w.h.watkinson@gmail.com>
2023-08-13 19:54:30 +0100

    failing test

---
+++ wiggle_query_language/clauses/make/parse_make/tests/test_parse_make_properties.py
@@ -0,0 +1,40 @@
+import pytest
+
+from testing.test_helpers import does_not_raise
+from wiggle_query_language.clauses.make.parse_make.parse_make_properties import (
+    make_properties,
+    string_to_correct_data_type,
+    string_to_list_data_type,
+)
+
+
+class TestParseMakeProperties:
+    @pytest.mark.parametrize(
+        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+    )
+    def test_make_properties(
+        self, test_stmt: str, expected_value: dict, exception
+    ) -> None:
+        with exception:
+            test = make_properties(test_stmt)
+            assert test == dict()
+
+    @pytest.mark.parametrize(
+        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+    )
+    def test_string_to_correct_data_type(
+        self, test_stmt: str, expected_value: dict, exception
+    ) -> None:
+        with exception:
+            test = string_to_correct_data_type(test_stmt)
+            assert test == dict()
+
+    @pytest.mark.parametrize(
+        "test_stmt, expected_value, exception", [pytest.param("", "", does_not_raise())]
+    )
+    def test_string_to_list_data_type(
+        self, test_stmt: str, expected_value: dict, exception
+    ) -> None:
+        with exception:
+            test = string_to_list_data_type(test_stmt)
+            assert test
