2e8c5e5e7477259f9b7126bf5ca2d8084545a7ed
William Watkinson <w.h.watkinson@gmail.com>
2023-08-14 18:14:39 +0100

    isort and black all files

--- wiggle_shell/core/select_dbms/__init__.py
+++ wiggle_shell/core/select_dbms/__init__.py
@@ -19,15 +19,15 @@ from wiggle_shell.core.select_dbms.select_wiggle_number_file import (
 
 __all__ = [
     "create_new_database",
-    "get_existing_db_file_path",
-    "create_new_wiggle_number_file",
-    "get_existing_wn_file_path",
-    "list_existing_dbms",
-    "get_and_display_available_dbms",
     "create_new_dbms",
-    "get_new_dbms_file_paths",
+    "create_new_wiggle_number_file",
+    "delete_dbms",
+    "get_and_display_available_dbms",
+    "get_existing_db_file_path",
     "get_existing_dbms",
     "get_existing_dbms_file_paths",
-    "delete_dbms",
+    "get_existing_wn_file_path",
+    "get_new_dbms_file_paths",
+    "list_existing_dbms",
     "select_dbms",
 ]
