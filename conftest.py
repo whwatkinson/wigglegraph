200a3655c04d2812b978bed7d0b51df108fd1e1d
William Watkinson <w.h.watkinson@gmail.com>
2023-08-16 10:08:57 +0100

    add clear_relationship_index_test fixture to conftest

--- conftest.py
+++ conftest.py
@@ -2,8 +2,15 @@ from typing import Generator
 
 import pytest
 
-from testing import DATABASE_TEST_FILE_PATH, WIGGLE_NUMBER_TEST_FILE_PATH
+from testing import (
+    DATABASE_TEST_FILE_PATH,
+    INDEXES_TEST_FILE_PATH,
+    WIGGLE_NUMBER_TEST_FILE_PATH,
+)
 from wiggle_query_language.graph.database.database import wipe_database
+from wiggle_query_language.graph.database.relationship_index import (
+    wipe_relationship_index,
+)
 
 
 @pytest.fixture
@@ -13,6 +20,15 @@ def clear_database_test() -> Generator:
     yield None
 
     wipe_database(DATABASE_TEST_FILE_PATH, im_sure=True)
+
+
+@pytest.fixture
+def clear_relationship_index_test() -> Generator:
+    wipe_relationship_index(INDEXES_TEST_FILE_PATH, im_sure=True)
+
+    yield None
+
+    wipe_relationship_index(INDEXES_TEST_FILE_PATH, im_sure=True)
 
 
 @pytest.fixture
@@ -29,11 +45,13 @@ def clear_wiggle_number_test() -> Generator:
 @pytest.fixture
 def clear_dbms_test() -> Generator:
     wipe_database(DATABASE_TEST_FILE_PATH, im_sure=True)
+    wipe_relationship_index(INDEXES_TEST_FILE_PATH, im_sure=True)
     with open(WIGGLE_NUMBER_TEST_FILE_PATH, "w") as file_handle:
         file_handle.write("0")
 
     yield None
 
     wipe_database(DATABASE_TEST_FILE_PATH, im_sure=True)
-    with open(WIGGLE_NUMBER_TEST_FILE_PATH, "w") as file_handle:
+    wipe_relationship_index(WIGGLE_NUMBER_TEST_FILE_PATH, im_sure=True)
+    with open(INDEXES_TEST_FILE_PATH, "w") as file_handle:
         file_handle.write("0")
---
+++ wiggle_query_language/graph/database/tests/test_relationship_index.py
@@ -0,0 +1,8 @@
+#
+# import pytest
+#
+# class TestRelationshipIndex:
+#
+#
+#     def test_relationship_index(self):
+#         pass
